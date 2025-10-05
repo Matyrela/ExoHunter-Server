5f4913cc6b8aa46972cc0d397219450746c40965
GonzaJuarez <gonzalojuarez185@gmail.com>
2025-10-05 13:03:27 -0300

    cors

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from controllers import model_controller
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:4200",
+    "https://exohunter.earth"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(model_controller.router, prefix="/api", tags=["model"])
 
